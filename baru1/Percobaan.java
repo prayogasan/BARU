2d10046000aa3b943a3c214b53ffcefe2e64f7ac
prayogasan <prayogasanarisakti@gmail.com>
2021-09-29 19:51:29 +0700

    Percobaan.java

---
+++ baru1/Percobaan.java
@@ -0,0 +1,8 @@
+public class Percobaan {
+
+	public static void main(String[] args) {
+		System.out.println("Nama	: Prayoga San Arisakti");
+		System.out.println("NIM		: 6706213060");
+		System.out.println("Kelas	: RPLA 4505");
+	}
+}
